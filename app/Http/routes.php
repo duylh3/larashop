86811eb0fd3049c97c665490e155c5b445944291
duylh3 <duylh3>
2016-01-06 00:13:52 +0700

    abc

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,3 +29,22 @@ Route::get('/', function () {
 Route::group(['middleware' => ['web']], function () {
     //
 });
+
+Route::get('/','FrontController@index');
+Route::get('/products','FrontController@products');
+Route::get('/products/details/{id}','FrontController@product_details');
+Route::get('/products/categories/{name}','FrontController@product_categories');
+Route::get('/products/brands/{name}/{category?}','FrontController@product_brands');
+Route::get('/blog','FrontController@blog');
+Route::get('/blog/post/{id}','FrontController@blog_post');
+Route::get('/contact-us','FrontController@contact_us');
+Route::get('/login','FrontController@login');
+Route::get('/logout','FrontController@logout');
+Route::get('/cart','FrontController@cart');
+Route::get('/checkout','FrontController@checkout');
+Route::get('/search/{query}','FrontController@search');
+
+Route::get('blade', function () {
+    $drinks = array('Vodka','Gin','Brandy');
+    return view('page',array('name' => 'The Raven','day' => 'Friday','drinks' => $drinks));
+});
